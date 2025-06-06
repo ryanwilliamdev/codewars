0f10ec21cceaf7c98e316b6e879d2a3d5cdc4668
raindevice <126584354+ryanwilliamdev@users.noreply.github.com>
2025-06-06 14:22:09 -0400

    create multiply.js
    create and solve 8kyu codewars challenge "Multiply" using JS

---
+++ 8kyu/multiply.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+function multiply(a, b){
+  a * b
+}
+
+// SOLUTION
+function multiply(a, b){
+  let answer = a * b;
+  return answer
+}
