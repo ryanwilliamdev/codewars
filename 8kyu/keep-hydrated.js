b3f0a21362692c6f5134972dfdd50dbb3bb2e8b9
raindevice <126584354+raindevice@users.noreply.github.com>
2025-06-26 10:27:19 -0400

    create keep-hydrated.js
    created and solved codewars 8kyu "Keep Hydrated!" challenge using JS

---
+++ 8kyu/keep-hydrated.js
@@ -0,0 +1,13 @@
+/* INSTRUCTIONS
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+*/
+
+// SOLUTION
+function litres(time) {
+  let litresPerHour = time * 0.5
+  return Math.floor(litresPerHour)
+}
