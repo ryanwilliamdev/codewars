4b2fabf6da384935b87665a0288d58900cb2f556
raindevice <126584354+raindevice@users.noreply.github.com>
2025-07-09 12:53:05 -0400

    update only-need-one.js

---
+++ 8kyu/only-need-one.js
@@ -0,0 +1,16 @@
+/* INSTRUCTIONS
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+a can contain numbers or strings. x can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+// SOLUTION
+function check(a, x) {
+  if (a.includes(x)) {
+    return true
+  } else {
+    return false
+  }
+}
