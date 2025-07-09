8885d87967fbf1cbbe4a6b712448e66cc07c65e9
raindevice <126584354+raindevice@users.noreply.github.com>
2025-07-09 12:48:39 -0400

    create sum-array.js
    create and solve codewars 8kyu sum-arrays challenge

---
+++ 8kyu/sum-arrays.js
@@ -0,0 +1,8 @@
+/* INSTRUCTIONS
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+*/
+
+// SOLUTION
+function sum (numbers) {
+  return numbers.reduce((acc, c) => acc + c, 0)
+}
