f7225680f983242ae6a34efe38b06b66a53df16d
ryanwilliamdev <rywilldav@gmail.com>
2025-10-22 11:21:50 -0400

    create and solve codewars 7kyu 'sum of odd numbers' using JS

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,16 @@
+/* INSTRUCTIONS
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+*/
+
+// MY SOLUTION
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3)
+}
