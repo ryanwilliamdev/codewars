66601506da26454510c9343bb4d396c0484f79e3
ryanwilliamdev <rywilldav@gmail.com>
2025-12-08 11:57:35 -0500

    Create a function that accepts a parameter and returns a message

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,8 @@
+/* INSTRUCTIONS
+Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+*/
+
+// MY SOLUTION
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
