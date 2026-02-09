b8e81f4befd018591ad972a9da202d1ad03b8763
ryanwilliamdev <rywilldav@gmail.com>
2026-02-09 09:43:41 -0500

    Given a list, return the list in reverse order.

---
+++ 8kyu/reverse-list-order.js
@@ -0,0 +1,12 @@
+/*
+INSTRUCTIONS
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+example:
+[1, 2, 3, 4]  -> [4, 3, 2, 1]
+*/
+
+// MY SOLUTION
+function reverseList(list) {
+  return list.reverse()
+}
